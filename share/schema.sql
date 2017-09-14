9cf868e703674960e35644064392c592f6054d52
Bernhard Fröhlich <decke@bluelife.at>
2017-09-14 11:16:25 +0200

    - Remove username from User table (obsoleted by email) - Convert CHAR() columns to VARCHAR() where appropriate

--- share/schema.sql
+++ share/schema.sql
@@ -8,8 +8,8 @@
 BEGIN TRANSACTION;
 
 CREATE TABLE config (
-   name CHAR(50) NOT NULL,
-   value CHAR(50) NOT NULL
+   name VARCHAR(50) NOT NULL,
+   value VARCHAR(50) NOT NULL
 );
 
 CREATE INDEX config_idx1 ON config (name);
@@ -17,37 +17,35 @@ CREATE INDEX config_idx1 ON config (name);
 
 CREATE TABLE hardware (
    hardwareid INTEGER PRIMARY KEY NOT NULL,
-   vendor CHAR(50) NOT NULL,
-   name CHAR(50) NOT NULL,
-   url CHAR(255) NOT NULL
+   vendor VARCHAR(50) NOT NULL,
+   name VARCHAR(50) NOT NULL,
+   url VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE users (
    userid INTEGER PRIMARY KEY NOT NULL,
-   username CHAR(50) NOT NULL,
-   password CHAR(255) NOT NULL,
-   email CHAR(255) NOT NULL,
-   firstname CHAR(50) NOT NULL,
-   lastname CHAR(50) NOT NULL,
-   phone CHAR(50) NOT NULL,
-   usergroup CHAR(10) NOT NULL -- permissions [user|admin]
+   email VARCHAR(50) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   firstname VARCHAR(50) NOT NULL,
+   lastname VARCHAR(50) NOT NULL,
+   phone VARCHAR(50) NOT NULL,
+   usergroup VARCHAR(10) NOT NULL -- permissions [user|admin]
 );
 
-CREATE UNIQUE INDEX users_idx1 ON users (username);
-CREATE UNIQUE INDEX users_idx2 ON users (email);
+CREATE UNIQUE INDEX users_idx1 ON users (email);
 
 
 CREATE TABLE locations (
    locationid INTEGER PRIMARY KEY NOT NULL,
-   name CHAR(50) NOT NULL,
+   name VARCHAR(50) NOT NULL,
    owner INTEGER  NOT NULL,
-   address CHAR(255) NOT NULL,
+   address VARCHAR(255) NOT NULL,
    latitude REAL NOT NULL,
    longitude REAL NOT NULL,
-   status CHAR(10) NOT NULL, -- current status: [online|offline|hidden]
-   gallerylink CHAR(255) NOT NULL,
-   description BLOB,
+   status VARCHAR(10) NOT NULL, -- current status: [online|offline|private]
+   gallerylink VARCHAR(255) NOT NULL,
+   description TEXT,
    FOREIGN KEY(owner) REFERENCES users(userid)
 );
 
@@ -57,11 +55,11 @@ CREATE INDEX locations_idx2 ON locations (owner);
 
 CREATE TABLE nodes (
    nodeid INTEGER PRIMARY KEY NOT NULL,
-   name CHAR(50) NOT NULL,
+   name VARCHAR(50) NOT NULL,
    owner INTEGER NOT NULL,
    location INTEGER NOT NULL,
    hardware INTEGER NOT NULL,
-   documentation BLOB,
+   description TEXT,
    FOREIGN KEY(owner) REFERENCES users(userid),
    FOREIGN KEY(location) REFERENCES locations(locationid),
    FOREIGN KEY(hardware) REFERENCES hardware(hardwareid)
@@ -75,14 +73,14 @@ CREATE INDEX nodes_idx4 ON nodes (hardware);
 
 CREATE TABLE interfaces (
    interfaceid INTEGER PRIMARY KEY NOT NULL,
-   name CHAR(50) NOT NULL,
+   name VARCHAR(50) NOT NULL,
    node INTEGER NOT NULL,
-   category CHAR(10) NOT NULL, -- interface category: [fiber|tunnel|wifi2.4|wifi5]
-   type CHAR(5) NOT NULL, -- interface type: [IPv4|IPv6]
-   address CHAR(50) NOT NULL,
-   status CHAR(10) NOT NULL, -- current OLSR status: [online|offline]
+   category VARCHAR(10) NOT NULL, -- interface category: [fiber|tunnel|wifi2.4|wifi5]
+   type VARCHAR(5) NOT NULL, -- interface type: [IPv4|IPv6]
+   address VARCHAR(50) NOT NULL,
+   status VARCHAR(10) NOT NULL, -- current OLSR status: [online|offline]
    ping INTEGER NOT NULL, -- smokeping: 0=disabled, 1=enabled
-   comment CHAR(255) NOT NULL,
+   description TEXT,
    FOREIGN KEY(node) REFERENCES nodes(nodeid)
 );
 
@@ -98,6 +96,7 @@ CREATE TABLE linkdata (
    fromif INTEGER NOT NULL,
    toif INTEGER NOT NULL,
    quality REAL NOT NULL,
+   source VARCHAR(10) NOT NULL, -- datasource: [olsrd|manual]
    FOREIGN KEY(fromif) REFERENCES interfaces(interfaceid),
    FOREIGN KEY(toif) REFERENCES interfaces(interfaceid)
 );
@@ -117,6 +116,6 @@ INSERT INTO "config" VALUES('title.url','https://graz.funkfeuer.at/');
 
 --- default account
 --- username: admin, password: admin
-INSERT INTO "users" VALUES(1,'admin','$2y$11$mHyBgtw2Iu0JuUpAvr.ChekNkRZMsLzmoH0/rJJQUYxEJjii.CFjS','noreply@example.com','','','','admin');
+INSERT INTO "users" VALUES(1,'admin@example.com','$2y$11$mHyBgtw2Iu0JuUpAvr.ChekNkRZMsLzmoH0/rJJQUYxEJjii.CFjS','','','','admin');
 
 COMMIT;
