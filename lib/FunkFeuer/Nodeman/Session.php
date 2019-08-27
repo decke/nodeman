36d1e42007fab0cc58548735f63e982e39e67878
Bernhard Froehlich <decke@bluelife.at>
2019-08-27 12:43:42 +0000

    Fix code formatting

--- lib/FunkFeuer/Nodeman/Session.php
+++ lib/FunkFeuer/Nodeman/Session.php
@@ -81,9 +81,14 @@ class Session
 
         /* also destroy session cookie on client */
         $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params['path'], $params['domain'],
-            $params['secure'], $params['httponly']
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params['path'],
+            $params['domain'],
+            $params['secure'],
+            $params['httponly']
         );
 
         session_destroy();
